6f24a77305c37b56c4efa77474f05f322f1383eb
trieu04 <115911417+trieu04@users.noreply.github.com>
2025-05-05 08:47:21 +0700

    feat: implement ui history selection in navigation component

--- src/app/scoring/nav/nav.tsx
+++ src/app/scoring/nav/nav.tsx
@@ -1,7 +1,9 @@
 import Icons from "@/components/icons";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export function NavComponent() {
+  const [pickedHistory, setPickedHistory] = useState<{ id: number; name: string } | null>(null);
   return (
     <div className="px-4 w-64 max-h-full overflow-y-auto">
       <div className="py-3 border-b-2 border-dscl-blue1 flex items-center">
@@ -15,32 +17,70 @@ export function NavComponent() {
         </Button>
       </div>
       <div>
-        <div className="flex justify-center border-b-2 border-dscl-blue1 py-2">
-          <h3 className="grow">History</h3>
-          <Icons.MoreVerticalIcon />
-        </div>
-        <div className="flex flex-col space-y-6 py-6">
-          {
-            [
-              { id: 1, name: "Nguyễn Đức Lộc" },
-              { id: 2, name: "Kíp 2, 09/10/2024" },
-              { id: 3, name: "Nguyễn Đức Lộc" },
-              { id: 4, name: "Kíp 2, 09/10/2024" },
-              { id: 5, name: "Nguyễn Đức Lộc" },
-              { id: 6, name: "Kíp 2, 09/10/2024" },
-              { id: 7, name: "Nguyễn Đức Lộc" },
-              { id: 8, name: "Kíp 2, 09/10/2024" },
-              { id: 9, name: "Nguyễn Đức Lộc" },
-              { id: 10, name: "Kíp 2, 09/10/2024" },
-              { id: 11, name: "Nguyễn Đức Lộc" },
-            ].map(item => (
-              <div className="flex items-center space-x-2" key={item.id}>
-                <Icons.ExamMultipleChoiceIcon />
-                <div>{item.name}</div>
-              </div>
-            ))
-          }
-        </div>
+        {
+          !pickedHistory
+            ? (
+                <>
+                  <div className="flex justify-center border-b-2 border-dscl-blue1 py-2">
+                    <h3 className="grow">History</h3>
+                    <Icons.MoreVerticalIcon />
+                  </div>
+                  <div className="flex flex-col space-y-6 py-6">
+                    {
+                      [
+                        { id: 2, name: "Kíp 1, 09/10/2024" },
+                        { id: 4, name: "Kíp 2, 09/10/2024" },
+                        { id: 6, name: "Kíp 3, 09/10/2024" },
+                        { id: 8, name: "Kíp 4, 09/10/2024" },
+                        { id: 10, name: "Kíp 1, 10/10/2024" },
+                      ].map(item => (
+                        <div key={item.id}>
+                          <div
+                            className="cursor-pointer flex items-center space-x-2"
+                            onClick={() => {
+                              setPickedHistory(item);
+                            }}
+                          >
+                            <Icons.ExamMultipleChoiceIcon />
+                            <div>{item.name}</div>
+                          </div>
+                        </div>
+                      ))
+                    }
+                  </div>
+                </>
+              )
+            : (
+                <>
+                  <div className="flex justify-center border-b-2 border-dscl-blue1 py-2">
+                    <Icons.ChevronLeftIcon onClick={() => setPickedHistory(null)} className="cursor-pointer mr-1" />
+                    <h3 className="grow">{pickedHistory.name}</h3>
+                    <Icons.MoreVerticalIcon />
+                  </div>
+                  <div className="flex flex-col space-y-6 py-6">
+                    {
+                      [
+                        { id: 2, name: "Phách 100" },
+                        { id: 4, name: "Phách 101" },
+                        { id: 6, name: "Phách 102" },
+                        { id: 8, name: "Phách 103" },
+                        { id: 10, name: "Phách 104" },
+                      ].map(item => (
+                        <div key={item.id}>
+                          <div
+                            className="flex items-center space-x-2 cursor-pointer"
+                          >
+                            <Icons.ExamMultipleChoiceIcon />
+                            <div>{item.name}</div>
+                          </div>
+                        </div>
+                      ))
+                    }
+                  </div>
+                </>
+              )
+        }
+
       </div>
     </div>
   );
